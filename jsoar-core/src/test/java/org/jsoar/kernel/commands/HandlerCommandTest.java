000f257db949cea996bd946f0238962944214656
markschenk <mark.schenk@independentip.com>
2021-06-09 06:52:23 +0200

    Made some small improvements to HandlerCommand

--- jsoar-core/src/test/java/org/jsoar/kernel/commands/HandlerCommandTest.java
+++ jsoar-core/src/test/java/org/jsoar/kernel/commands/HandlerCommandTest.java
@@ -1,7 +1,10 @@
 package org.jsoar.kernel.commands;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.io.StringWriter;
 import java.util.regex.Pattern;
@@ -10,6 +13,8 @@ import org.jsoar.kernel.LogManager;
 import org.jsoar.kernel.LogManager.EchoMode;
 import org.jsoar.kernel.LogManager.LogLevel;
 import org.jsoar.kernel.RunType;
+import org.jsoar.kernel.SoarException;
+import org.jsoar.kernel.rhs.functions.RhsFunctionHandler;
 import org.jsoar.kernel.rhs.functions.RhsFunctionManager;
 import org.jsoar.util.commands.DefaultSoarCommandContext;
 import org.junit.After;
@@ -17,14 +22,17 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class HandlerCommandTest {
+
   private Agent agent;
   private StringWriter outputWriter = new StringWriter();
+  HandlerCommand handlerCommand;
 
   @Before
   public void setUp() throws Exception {
     agent = new Agent();
-
     agent.getPrinter().addPersistentWriter(outputWriter);
+
+    handlerCommand = new HandlerCommand(agent);
   }
 
   @After
@@ -58,7 +66,6 @@ public class HandlerCommandTest {
     // Variables
     Pattern regex = Pattern.compile("^Simple test$", Pattern.MULTILINE);
     LogManager logManager = agent.getLogManager();
-    HandlerCommand handlerCommand = new HandlerCommand(agent);
 
     // Configure the logger
     logManager.setEchoMode(EchoMode.simple);
@@ -67,7 +74,7 @@ public class HandlerCommandTest {
 
     // Test enabled.
     handlerCommand.execute(
-        DefaultSoarCommandContext.empty(), new String[] {"handler", "--enable", "log"});
+        DefaultSoarCommandContext.empty(), new String[]{"handler", "--enable", "log"});
     agent
         .getProductions()
         .loadProduction("test (state <s> ^superstate nil) --> (log info |Simple test|)");
@@ -77,7 +84,7 @@ public class HandlerCommandTest {
 
     // Test disabled.
     handlerCommand.execute(
-        DefaultSoarCommandContext.empty(), new String[] {"handler", "--disable", "log"});
+        DefaultSoarCommandContext.empty(), new String[]{"handler", "--disable", "log"});
     agent
         .getProductions()
         .loadProduction("test2 (state <s> ^superstate nil) --> (log info |Simple test|)");
@@ -87,7 +94,7 @@ public class HandlerCommandTest {
 
     // Test enabled.
     handlerCommand.execute(
-        DefaultSoarCommandContext.empty(), new String[] {"handler", "--enable", "log"});
+        DefaultSoarCommandContext.empty(), new String[]{"handler", "--enable", "log"});
     agent
         .getProductions()
         .loadProduction("test3 (state <s> ^superstate nil) --> (log info |Simple test|)");
@@ -99,22 +106,51 @@ public class HandlerCommandTest {
   @Test
   public void testHandlerList() throws Exception {
     Pattern regex = Pattern.compile("^\\s*log\\s*$", Pattern.MULTILINE);
-    HandlerCommand handlerCommand = new HandlerCommand(agent);
     String result;
 
     handlerCommand.execute(
-        DefaultSoarCommandContext.empty(), new String[] {"handler", "--enable", "log"});
-    result = handlerCommand.execute(DefaultSoarCommandContext.empty(), new String[] {"handler"});
+        DefaultSoarCommandContext.empty(), new String[]{"handler", "--enable", "log"});
+    result = handlerCommand.execute(DefaultSoarCommandContext.empty(), new String[]{"handler"});
     assertFalse(regex.matcher(result).find());
 
     handlerCommand.execute(
-        DefaultSoarCommandContext.empty(), new String[] {"handler", "--disable", "log"});
-    result = handlerCommand.execute(DefaultSoarCommandContext.empty(), new String[] {"handler"});
+        DefaultSoarCommandContext.empty(), new String[]{"handler", "--disable", "log"});
+    result = handlerCommand.execute(DefaultSoarCommandContext.empty(), new String[]{"handler"});
     assertTrue(regex.matcher(result).find());
 
     handlerCommand.execute(
-        DefaultSoarCommandContext.empty(), new String[] {"handler", "--enable", "log"});
-    result = handlerCommand.execute(DefaultSoarCommandContext.empty(), new String[] {"handler"});
+        DefaultSoarCommandContext.empty(), new String[]{"handler", "--enable", "log"});
+    result = handlerCommand.execute(DefaultSoarCommandContext.empty(), new String[]{"handler"});
     assertFalse(regex.matcher(result).find());
+  }
+
+  @Test
+  public void testPrintDisabledRhsFunctions() throws SoarException {
+    // Given disabled handlers
+    RhsFunctionManager rhsFunctionManager = agent.getRhsFunctions();
+    registerRhsHandler("RHS-FUNCTION");
+    registerRhsHandler("OTHER-RHS-FUNCTION");
+    rhsFunctionManager.disableHandler("RHS-FUNCTION");
+    rhsFunctionManager.disableHandler("OTHER-RHS-FUNCTION");
+
+    // When printing disabled rhs function handlers
+    String result = handlerCommand.execute(
+        DefaultSoarCommandContext.empty(), new String[]{"handler"});
+
+    // Then result contains disabled RHS functions
+    assertEquals("""
+        ===== Disabled RHS Functions =====
+        OTHER-RHS-FUNCTION
+        RHS-FUNCTION
+        """, result);
+  }
+
+  private void registerRhsHandler(final String name) {
+    RhsFunctionManager rhsFunctionManager = agent.getRhsFunctions();
+
+    RhsFunctionHandler rhsFunctionHandler = mock(RhsFunctionHandler.class);
+    when(rhsFunctionHandler.getName()).thenReturn(name);
+
+    rhsFunctionManager.registerHandler(rhsFunctionHandler);
   }
 }
